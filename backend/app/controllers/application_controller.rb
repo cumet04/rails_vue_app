b3988fb2c0f6da1792888553ef5e16d5d9de38d1
cumet04 <medalhkr@gmail.com>
2020-04-24 21:12:54 +0900

    silence 404 error

--- backend/app/controllers/application_controller.rb
+++ backend/app/controllers/application_controller.rb
@@ -1,2 +1,5 @@
 class ApplicationController < ActionController::Base
+  def render_404(e = nil)
+    head 404
+  end
 end
--- backend/config/routes.rb
+++ backend/config/routes.rb
@@ -1,3 +1,3 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  get "*path" => "application#render_404"
 end
