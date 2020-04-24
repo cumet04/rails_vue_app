a845a9e2bd5ef19da9ef61f408b96d5c941db536
cumet04 <medalhkr@gmail.com>
2020-04-24 15:58:16 +0900

    minimize dev log

--- backend/config/environments/development.rb
+++ backend/config/environments/development.rb
@@ -6,6 +6,13 @@ Rails.application.configure do
   config.action_controller.perform_caching = false
   config.cache_store = :null_store
 
+  config.log_level = :debug
+  config.log_tags = []
+  config.log_formatter = proc do |severity, datetime, progname, msg|
+    time = datetime.strftime("%H:%M:%S:%L")
+    "[#{severity}] #{time} : #{msg}\n"
+  end
+
   config.active_support.deprecation = :log
   config.active_record.migration_error = :page_load
   config.active_record.verbose_query_logs = true
