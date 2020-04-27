b1fc90260b93d541ea1bda796fd8908c845ea4a9
cumet04 <medalhkr@gmail.com>
2020-04-28 02:51:39 +0900

    ress

--- frontend/webpack.config.js
+++ frontend/webpack.config.js
@@ -32,7 +32,19 @@ module.exports = {
       },
       {
         test: /\.scss$/,
-        use: ["vue-style-loader", "css-loader", "sass-loader"],
+        use: [
+          "vue-style-loader",
+          "css-loader",
+          {
+            loader: "sass-loader",
+            options: {
+              prependData: `@import "${path.resolve(
+                __dirname,
+                "assets/css/common"
+              )}";`,
+            },
+          },
+        ],
       },
     ],
   },
