357cd2c1550484da3492b5fa786cbdb4240c5ac5
cumet04 <medalhkr@gmail.com>
2020-04-30 01:54:45 +0900

    fix npm dev

--- frontend/lib/watch-dev.js
+++ frontend/lib/watch-dev.js
@@ -1,7 +1,7 @@
 const chokidar = require("chokidar");
-const gen_imports = require("./gen_imports");
+const genImports = require("./gen-imports");
 
 chokidar.watch("src/pages", { atomic: true }).on("all", (event, path) => {
   const add_or_remove = event == "add" || event == "unlink";
-  if (add_or_remove && path.endsWith(".vue")) gen_imports();
+  if (add_or_remove && path.endsWith(".vue")) genImports();
 });
