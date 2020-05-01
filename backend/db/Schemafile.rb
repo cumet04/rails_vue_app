7d1523aef8a039a733ac39e0e51e47a132648ea1
cumet04 <medalhkr@gmail.com>
2020-05-01 19:43:16 +0900

    use ridgepole

---
+++ backend/db/Schemafile.rb
@@ -0,0 +1,25 @@
+# -*- mode: ruby -*-
+# vi: set ft=ruby :
+create_table "posts", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+  t.string "title", null: false
+  t.text "content"
+  t.bigint "author_id", null: false
+  t.datetime "deleted_at"
+  t.virtual "is_available", type: :boolean, as: "if(`deleted_at` is null,1,NULL)"
+  t.datetime "created_at", precision: 6, null: false
+  t.datetime "updated_at", precision: 6, null: false
+  t.index ["author_id"], name: "index_posts_on_author_id"
+  t.index ["title"], name: "index_posts_on_title"
+end
+
+create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+  t.string "email", null: false
+  t.string "encrypted_password", null: false
+  t.datetime "created_at", precision: 6, null: false
+  t.datetime "updated_at", precision: 6, null: false
+  t.datetime "deleted_at"
+  t.virtual "is_available", type: :boolean, as: "if(`deleted_at` is null,1,NULL)"
+  t.index ["email", "is_available"], name: "index_users_on_email_and_is_available", unique: true
+end
+
+add_foreign_key "posts", "users", column: "author_id"
---
+++ backend/lib/tasks/ridgepole.rake
@@ -0,0 +1,41 @@
+namespace :ridgepole do
+  desc "Apply database schema"
+  task apply: :environment do
+    ridgepole("--apply", "--file #{schema_file}")
+
+    if Rails.application.config.active_record.dump_schema_after_migration
+      Rake::Task["db:schema:dump"].invoke
+    end
+    Rake::Task["annotate_models"].invoke
+  end
+
+  desc "Export database schema"
+  task export: :environment do
+    ridgepole("--export", "--output #{schema_file}")
+  end
+
+  desc "Display diff between database and schema"
+  task diff: :environment do
+    ridgepole("--diff #{config_file} #{schema_file}")
+    exit($?.exitstatus)
+  end
+
+  private
+
+  def schema_file
+    Rails.root.join("db/Schemafile.rb") # with ".rb" for syntax highlight
+  end
+
+  def config_file
+    Rails.root.join("config/database.yml")
+  end
+
+  def ridgepole(*options)
+    command = [
+      "bundle exec ridgepole",
+      "--config #{config_file}",
+      "-E #{Rails.env}",
+    ]
+    system [command + options].join(" ")
+  end
+end
