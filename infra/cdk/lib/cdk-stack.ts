697d48bfebd3e7b64dd066a9f9125f97158875d3
cumet04 <medalhkr@gmail.com>
2020-05-06 04:42:36 +0900

    cdk; only app

--- infra/cdk/lib/cdk-stack.ts
+++ infra/cdk/lib/cdk-stack.ts
@@ -2,6 +2,8 @@ import * as cdk from "@aws-cdk/core";
 import * as ec2 from "@aws-cdk/aws-ec2";
 import * as elbv2 from "@aws-cdk/aws-elasticloadbalancingv2";
 import * as ecs from "@aws-cdk/aws-ecs";
+import * as ecr from "@aws-cdk/aws-ecr";
+import * as iam from "@aws-cdk/aws-iam";
 
 export class CdkStack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
@@ -15,19 +17,15 @@ export class CdkStack extends cdk.Stack {
         {
           name: "Ingress",
           subnetType: ec2.SubnetType.PUBLIC,
-          cidrMask: 28,
         },
         {
           name: "Data",
           subnetType: ec2.SubnetType.ISOLATED,
-          cidrMask: 28,
         },
         {
           name: "App",
           subnetType: ec2.SubnetType.ISOLATED,
-          cidrMask: 26,
         },
-        // rest: one of 27
       ],
     });
 
@@ -44,14 +42,20 @@ export class CdkStack extends cdk.Stack {
     const sgRedis = new ec2.SecurityGroup(this, "sgRDS", { vpc });
     sgRedis.addIngressRule(sgApp, ec2.Port.tcp(3306));
 
+    const sgRepository = new ec2.SecurityGroup(this, "sgRepository", { vpc });
+    sgRepository.addIngressRule(ec2.Peer.anyIpv4(), ec2.Port.tcp(443));
+
     // --- ALB ---
     const alb = new elbv2.ApplicationLoadBalancer(this, "ALB", {
       vpc,
+      vpcSubnets: vpc.selectSubnets({ subnetGroupName: "Ingress" }),
       internetFacing: true,
+      securityGroup: sgALB,
     });
     alb
       .addListener("albListner80", { port: 80 })
       .addRedirectResponse("albListener80RedirectResponse", {
+        protocol: "HTTPS",
         port: "443",
         statusCode: "HTTP_301",
       });
@@ -69,12 +73,26 @@ export class CdkStack extends cdk.Stack {
         targetGroups: [
           new elbv2.ApplicationTargetGroup(this, "albtgApp", {
             vpc,
+            targetType: elbv2.TargetType.IP,
             protocol: elbv2.ApplicationProtocol.HTTP,
             port: 80,
-            // MEMO: no default targets
+            // NOTE: no default targets
           }),
         ],
       });
+
+    // --- ECR --
+    const repository = new ecr.Repository(this, "ecrRepository");
+    // For Fargate in isolated subnet, ECR_DOCKER and S3 private endpoint are needed.
+    // refs https://docs.aws.amazon.com/AmazonECR/latest/userguide/vpc-endpoints.html
+    vpc.addInterfaceEndpoint("vpcEcrEndpoint", {
+      service: ec2.InterfaceVpcEndpointAwsService.ECR_DOCKER,
+      subnets: vpc.selectSubnets({ subnetGroupName: "App" }),
+      securityGroups: [sgRepository],
+    });
+    vpc.addS3Endpoint("vpcS3Endpoint", [
+      vpc.selectSubnets({ subnetGroupName: "App" }),
+    ]);
 
     // --- ECS ---
     const cluster = new ecs.Cluster(this, "Cluster", { vpc });
@@ -82,14 +100,25 @@ export class CdkStack extends cdk.Stack {
       compatibility: ecs.Compatibility.FARGATE,
       cpu: "256",
       memoryMiB: "512",
-      // executionRole: for using ECR
+      executionRole: new iam.Role(this, "roleTaskExecution", {
+        assumedBy: new iam.ServicePrincipal("ecs-tasks.amazonaws.com"),
+        managedPolicies: [
+          iam.ManagedPolicy.fromAwsManagedPolicyName(
+            "service-role/AmazonECSTaskExecutionRolePolicy"
+          ),
+        ],
+      }),
     });
-    taskDefinition.addContainer("ecsContanerApp", {
-      image: ecs.RepositoryImage.fromRegistry("httpd"),
-    });
+    taskDefinition
+      .addContainer("ecsContanerApp", {
+        image: ecs.ContainerImage.fromEcrRepository(repository),
+      })
+      .addPortMappings({ containerPort: 80 });
     new ecs.FargateService(this, "ecsFargateService", {
       cluster,
       taskDefinition,
+      vpcSubnets: vpc.selectSubnets({ subnetGroupName: "App" }),
+      securityGroup: sgApp,
     });
   }
 }
