e4d16b60a52cbdcb89da4c5268b79c76066cb491
cumet04 <medalhkr@gmail.com>
2020-05-07 16:33:49 +0900

    cdk datastore

--- infra/cdk/lib/cdk-stack.ts
+++ infra/cdk/lib/cdk-stack.ts
@@ -4,30 +4,41 @@ import * as elbv2 from "@aws-cdk/aws-elasticloadbalancingv2";
 import * as ecs from "@aws-cdk/aws-ecs";
 import * as ecr from "@aws-cdk/aws-ecr";
 import * as iam from "@aws-cdk/aws-iam";
+import * as elasticache from "@aws-cdk/aws-elasticache";
+import * as rds from "@aws-cdk/aws-rds";
 
 export class CdkStack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    const { vpc, sgALB, sgApp, sgDB, sgRedis, sgRepository } = this.createVpc();
-
-    this.createALB(
+    const {
       vpc,
+      snIngress,
+      snApp,
+      snData,
       sgALB,
-      vpc.selectSubnets({ subnetGroupName: "Ingress" })
-    );
-
-    const repository = this.createECR(
-      vpc,
+      sgApp,
+      sgDB,
+      sgRedis,
       sgRepository,
-      vpc.selectSubnets({ subnetGroupName: "App" })
-    );
+    } = this.createVpc();
+
+    this.createALB(vpc, sgALB, snIngress);
+
+    const repository = this.createECR(vpc, sgRepository, snApp);
 
     this.createECS(vpc, sgApp, repository);
+
+    this.createRedis(sgRedis, snData);
+
+    this.createRDS(vpc, sgDB, snData);
   }
 
   createVpc(): {
     vpc: ec2.Vpc;
+    snIngress: ec2.SubnetSelection;
+    snApp: ec2.SubnetSelection;
+    snData: ec2.SubnetSelection;
     sgALB: ec2.SecurityGroup;
     sgApp: ec2.SecurityGroup;
     sgDB: ec2.SecurityGroup;
@@ -63,13 +74,23 @@ export class CdkStack extends cdk.Stack {
     const sgDB = new ec2.SecurityGroup(this, "sgDB", { vpc });
     sgDB.addIngressRule(sgApp, ec2.Port.tcp(3306));
 
-    const sgRedis = new ec2.SecurityGroup(this, "sgRDS", { vpc });
+    const sgRedis = new ec2.SecurityGroup(this, "sgRedis", { vpc });
     sgRedis.addIngressRule(sgApp, ec2.Port.tcp(3306));
 
     const sgRepository = new ec2.SecurityGroup(this, "sgRepository", { vpc });
     sgRepository.addIngressRule(ec2.Peer.anyIpv4(), ec2.Port.tcp(443));
 
-    return { vpc, sgALB, sgApp, sgDB, sgRedis, sgRepository };
+    return {
+      vpc,
+      snIngress: vpc.selectSubnets({ subnetGroupName: "Ingress" }),
+      snApp: vpc.selectSubnets({ subnetGroupName: "App" }),
+      snData: vpc.selectSubnets({ subnetGroupName: "Data" }),
+      sgALB,
+      sgApp,
+      sgDB,
+      sgRedis,
+      sgRepository,
+    };
   }
 
   createALB(vpc: ec2.Vpc, sg: ec2.SecurityGroup, subnets: ec2.SubnetSelection) {
@@ -152,6 +173,52 @@ export class CdkStack extends cdk.Stack {
       taskDefinition,
       vpcSubnets: vpc.selectSubnets({ subnetGroupName: "App" }),
       securityGroup: sg,
+    });
+  }
+
+  createRedis(sg: ec2.SecurityGroup, subnets: ec2.SubnetSelection) {
+    const snGroup = new elasticache.CfnSubnetGroup(this, "cacheSubnetGroup", {
+      description: "cache subnet group",
+      subnetIds: subnets.subnets?.map((sn) => sn.subnetId) || [],
+    });
+    snGroup.cacheSubnetGroupName;
+    new elasticache.CfnCacheCluster(this, "Redis", {
+      azMode: "single-az",
+      cacheNodeType: "cache.t3.micro",
+      cacheSubnetGroupName: snGroup.ref, // with snGroup.cacheSubnetGroupName, it dooesn't work
+      engine: "redis",
+      engineVersion: "5.0.6",
+      numCacheNodes: 1,
+      vpcSecurityGroupIds: [sg.securityGroupId],
+    });
+  }
+
+  createRDS(vpc: ec2.Vpc, sg: ec2.SecurityGroup, subnets: ec2.SubnetSelection) {
+    const db = new rds.DatabaseInstance(this, "RDS", {
+      vpc,
+      vpcPlacement: subnets,
+      securityGroups: [sg],
+      engine: rds.DatabaseInstanceEngine.MARIADB,
+      engineVersion: "10.4",
+      instanceClass: ec2.InstanceType.of(
+        ec2.InstanceClass.T3,
+        ec2.InstanceSize.MICRO
+      ),
+      multiAz: false,
+      masterUsername: "raislapp",
+      databaseName: "railsapp",
+      allocatedStorage: 10,
+      parameterGroup: new rds.ParameterGroup(this, "rdsParamGroup", {
+        family: "mariadb10.4",
+        parameters: {
+          character_set_client: "utf8mb4",
+          character_set_connection: "utf8mb4",
+          character_set_database: "utf8mb4",
+          character_set_server: "utf8mb4",
+          collation_connection: "utf8mb4_bin",
+          collation_server: "utf8mb4_bin",
+        },
+      }),
     });
   }
 }
