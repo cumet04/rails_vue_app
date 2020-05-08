5f529fa2a098e4289a922931ecb1fcff63aa2e34
cumet04 <medalhkr@gmail.com>
2020-05-08 13:55:14 +0900

    docker env

--- frontend/webpack.config.js
+++ frontend/webpack.config.js
@@ -8,19 +8,16 @@ const assets_path = "/assets";
 module.exports = {
   entry: ["./src/index.js"],
   output: {
-    path: path.resolve(__dirname, `../backend/public${assets_path}`),
+    path: path.resolve(__dirname, `../nginx/public${assets_path}`),
     filename: "bundle.js",
   },
   mode: "production",
   devServer: {
     port,
-    contentBase: path.resolve(__dirname, "public"),
-    headers: {
-      "Access-Control-Allow-Origin": "*",
-      "Access-Control-Allow-Methods": "GET",
-      "Access-Control-Allow-Headers":
-        "X-Requested-With, content-type, Authorization",
-    },
+    host: "0.0.0.0",
+    disableHostCheck: true,
+    publicPath: assets_path,
+    sockPath: `${assets_path}/sockjs-node`,
   },
   module: {
     rules: [
@@ -63,10 +60,7 @@ module.exports = {
   plugins: [
     new VueLoaderPlugin(),
     new webpack.DefinePlugin({
-      ASSETS_PATH:
-        process.env.NODE_ENV == "development"
-          ? JSON.stringify(`http://localhost:${port}`)
-          : JSON.stringify(assets_path),
+      ASSETS_PATH: JSON.stringify(assets_path),
     }),
   ],
 };
